7e71132668af22ec1c10bd727439608e71f23e9c
SeoulSKY <swkim7519@protonmail.ch>
2023-05-26 15:38:11 +0900

    Setup initial API

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,21 @@ mod board;
 mod errors;
 mod game;
 mod bot;
+mod api;
 
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[rocket::main]
+async fn main() -> Result<(), rocket::Error> {
+    rocket::build()
+        .mount("/", routes![index])
+        .launch()
+        .await?;
+
+    Ok(())
 }
