4f9042abc00bd58b60c512954d59d91152b4b598
SeoulSKY <swkim7519@protonmail.ch>
2023-08-12 17:42:27 -0600

    Did refactorings

--- client/othello/Assets/Scripts/Play/Disk.cs
+++ client/othello/Assets/Scripts/Play/Disk.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Collections;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
--- client/othello/Assets/Scripts/Play/Tile.cs
+++ client/othello/Assets/Scripts/Play/Tile.cs
@@ -1,6 +1,4 @@
 using System;
-using System.Collections;
-using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using JetBrains.Annotations;
 using UnityEngine;
