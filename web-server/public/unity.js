c0d6c7d935e62ea3b704634d4beb9f75e5b10f6e
SeoulSKY <email@seoulsky.org>
2023-12-28 22:43:30 -0600

    Update unity.js

--- web-server/public/unity.js
+++ web-server/public/unity.js
@@ -1,13 +1,11 @@
 function main() {
     if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
-        const meta = document.createElement("meta");
-        meta.name = "viewport";
-        meta.content = "width=device-width, height=device-height, initial-scale=1.0, user-scalable=no, shrink-to-fit=yes";
-        document.getElementsByTagName("head")[0].appendChild(meta);
+        alert("Mobile devices are not supported. Please use a desktop.");
+        return;
     }
 
     const canvas = document.querySelector("#unity-canvas");
-    if (canvas == null) {
+    if (canvas === null) {
         throw new Error("Cannot find canvas element");
     }
 
@@ -17,32 +15,39 @@ function main() {
 
     document.body.style.textAlign = "left";
 
-    let unity = new UnityWebgl("#unity-canvas", {
-        loaderUrl: "Build/WebGL.loader.js",
-        dataUrl: "Build/WebGL.data.unityweb",
-        frameworkUrl: "Build/WebGL.framework.js.unityweb",
-        codeUrl: "Build/WebGL.wasm.unityweb",
-        streamingAssetsUrl: "StreamingAssets",
-        companyName: "SeoulSKY",
-        productName: "Desdemona",
-        productVersion: "0.1.0",
-    });
+    fetch("https://api.github.com/repos/seoulsky/desdemona/releases/latest")
+        .then(response => {
+            let unity = new UnityWebgl("#unity-canvas", {
+                loaderUrl: response["assets"].filter(asset => asset["name"] === "WebGL.loader.js")[0]["browser_download_url"],
+                dataUrl: response["assets"].filter(asset => asset["name"] === "WebGL.data.unityweb")[0]["browser_download_url"],
+                frameworkUrl: response["assets"].filter(asset => asset["name"] === "WebGL.framework.js.unityweb")[0]["browser_download_url"],
+                codeUrl: response["assets"].filter(asset => asset["name"] === "WebGL.wasm.unityweb")[0]["browser_download_url"],
+                streamingAssetsUrl: "StreamingAssets",
+                companyName: "SeoulSKY",
+                productName: "Desdemona",
+                productVersion: response["tag_name"],
+            });
 
-    let progressBar = document.getElementById("progress-bar");
+            let progressBar = document.getElementById("progress-bar");
 
-    unity.on("progress", (progression) => {
-        progressBar.style.width = 100 * progression + "%";
-    });
+            unity.on("progress", (progression) => {
+                progressBar.style.width = 100 * progression + "%";
+            });
 
-    unity.on("mounted", () => {
-        let spinner = document.getElementById("spinner");
-        spinner.style.display = "none";
+            unity.on("mounted", () => {
+                let spinner = document.getElementById("spinner");
+                spinner.style.display = "none";
 
-        progressBar.style.display = "none";
+                progressBar.style.display = "none";
 
-        let canvas = document.getElementById("unity-canvas");
-        canvas.style.display = "block";
-    });
+                let canvas = document.getElementById("unity-canvas");
+                canvas.style.display = "block";
+            });
+
+        }).catch(error => {
+            alert("Failed to load the game. Please try again later.");
+            console.error(error);
+        });
 }
 
 
